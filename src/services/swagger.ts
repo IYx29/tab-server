fe0c9de980e9632f37a10e49e777e4edea9493e0
IYx29 <125562414+IYx29@users.noreply.github.com>
2023-02-16 13:58:28 +0200

    Update swagger.ts

--- src/services/swagger.ts
+++ src/services/swagger.ts
@@ -8,7 +8,7 @@ const options: swaggerJsdoc.Options = {
     openapi: "3.0.0",
     info: {
       title: "Airbnb Clone REST API Docs",
-      version,
+      version : 1.0.0,
     },
     components: {
       securitySchemes: {
